0214f31d67ac70897c32cccb2fa4dc459e9d82e9
William Cooper <wcooperhockey@gmail.com>
2020-09-10 18:24:41 -0400

    reverted

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
   return (
     <div className="App">
-      <header>
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>We now have Auth!</h1>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
-      <AmplifySignOut />
     </div>
   );
 }
 
-export default withAuthenticator(App);
+export default App;
